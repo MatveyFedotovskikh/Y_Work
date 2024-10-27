7cbeccbc5f5af307d63825d6d27fea2688830f7a
MatveyFedotovskikh <mfudotovskikh@gmail.com>
2024-10-27 23:02:44 +0500

    dc

--- past_number.py
+++ past_number.py
@@ -3,11 +3,10 @@ def del_not_p_number(list_number,number_p,n):
     if number_p == 5:
         return del_not_p_number(list_number,list_number[list_number.index(number_p)+1],n)
     if number_p>(n**(0.5)):
-        a = list_number
-        return len(list_number)
+        return len(list_number)+1
     
     for number in list_number[:]:
-        if number*number_p in list_number:
+        if number*number_p in list_number[list_number.index(number_p)-1:]:
             list_number.remove(number*number_p)
         if number*number_p> n:
             break
@@ -19,10 +18,10 @@ def search_count_numbers(n):
     elif n<3:
         return 1
     list_number = [2, 3]
-    for i in range(9, n+1, 2):
+    for i in range(5, n+1, 2):
         if i%10 != 5:
             list_number.append(i)
     return del_not_p_number(list_number,3,n)
 
-print(search_count_numbers(1))
+print(search_count_numbers(5))
 
